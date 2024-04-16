77ac516a4d366f9a03a55df2c64656535ca7fa38
Sicelumusa <musaqwabe@gmail.com>
2024-04-16 09:35:36 +0000

    Added project files up to task 5

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -6,12 +6,52 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# Initialize auth to None
+auth = None
+
+# Load and assign the right instance of authentication to auth
+auth_type = getenv('AUTH_TYPE')
+if auth_type and auth_type == 'custom':
+    auth = Auth()
+
+# excluded paths
+excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+
+@app.before_request
+def before_request():
+    global auth
+
+    print(auth)
+    print(request.path)
+
+    # If auth is None or request path is not in excluded path, do nothing
+    if auth is None:
+        return
+
+    # Check if authentication is required for the request path
+    if request.path not in excluded_paths and auth.require_auth(request.path, excluded_paths):
+        # Get the authorization header
+        auth_header = auth.authorization_header(request)
+
+        # If authorization header is None, raise 401 error
+        if auth_header is None:
+            abort(401)
+
+        # Check current user
+        current_user = auth.current_user(request)
+
+        # If current user is None, raise 403 error
+        if current_user is None:
+            abort(403)
 
 
 @app.errorhandler(404)
@@ -21,7 +61,19 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run( debug=True, host=host, port=port)
